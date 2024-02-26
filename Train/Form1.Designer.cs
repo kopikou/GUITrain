6305a2aaa68ae841ce7433cba35631e753542c24
Kopikou <copilowa.nadezhda2015@yandex.ru>
2024-02-26 16:49:57 +0800

    work

--- Train/Form1.Designer.cs
+++ Train/Form1.Designer.cs
@@ -28,29 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.arrivalHour = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.arrivalMinute = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.commingHour = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.commingMinute = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.departHour = new System.Windows.Forms.TextBox();
+            this.departMinute = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // arrivalHour
             // 
-            this.textBox1.Location = new System.Drawing.Point(180, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.arrivalHour.Location = new System.Drawing.Point(180, 30);
+            this.arrivalHour.Name = "arrivalHour";
+            this.arrivalHour.Size = new System.Drawing.Size(100, 22);
+            this.arrivalHour.TabIndex = 0;
             // 
             // label1
             // 
@@ -70,12 +71,12 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "часов";
             // 
-            // textBox2
+            // arrivalMinute
             // 
-            this.textBox2.Location = new System.Drawing.Point(367, 33);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 3;
+            this.arrivalMinute.Location = new System.Drawing.Point(367, 33);
+            this.arrivalMinute.Name = "arrivalMinute";
+            this.arrivalMinute.Size = new System.Drawing.Size(100, 22);
+            this.arrivalMinute.TabIndex = 3;
             // 
             // label3
             // 
@@ -95,13 +96,13 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Пассажир пришел в";
             // 
-            // textBox3
+            // commingHour
             // 
-            this.textBox3.Location = new System.Drawing.Point(180, 150);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.commingHour.Location = new System.Drawing.Point(180, 150);
+            this.commingHour.Name = "commingHour";
+            this.commingHour.Size = new System.Drawing.Size(100, 22);
+            this.commingHour.TabIndex = 6;
+            this.commingHour.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label5
             // 
@@ -112,12 +113,12 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "часов";
             // 
-            // textBox4
+            // commingMinute
             // 
-            this.textBox4.Location = new System.Drawing.Point(367, 150);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 8;
+            this.commingMinute.Location = new System.Drawing.Point(367, 150);
+            this.commingMinute.Name = "commingMinute";
+            this.commingMinute.Size = new System.Drawing.Size(100, 22);
+            this.commingMinute.TabIndex = 8;
             // 
             // label6
             // 
@@ -155,40 +156,51 @@
             this.label9.TabIndex = 12;
             this.label9.Text = "минут";
             // 
-            // textBox5
+            // departHour
             // 
-            this.textBox5.Location = new System.Drawing.Point(180, 96);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
-            this.textBox5.TabIndex = 13;
+            this.departHour.Location = new System.Drawing.Point(180, 96);
+            this.departHour.Name = "departHour";
+            this.departHour.Size = new System.Drawing.Size(100, 22);
+            this.departHour.TabIndex = 13;
             // 
-            // textBox6
+            // departMinute
             // 
-            this.textBox6.Location = new System.Drawing.Point(367, 96);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
-            this.textBox6.TabIndex = 14;
+            this.departMinute.Location = new System.Drawing.Point(367, 96);
+            this.departMinute.Name = "departMinute";
+            this.departMinute.Size = new System.Drawing.Size(100, 22);
+            this.departMinute.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(529, 36);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Будет ли поезд стоять на платформе?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
+            this.ClientSize = new System.Drawing.Size(563, 244);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.departMinute);
+            this.Controls.Add(this.departHour);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.commingMinute);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.commingHour);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.arrivalMinute);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.arrivalHour);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -198,21 +210,22 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox arrivalHour;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox arrivalMinute;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox commingHour;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox commingMinute;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox departHour;
+        private System.Windows.Forms.TextBox departMinute;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Train/Form1.cs
+++ Train/Form1.cs
@@ -15,11 +15,65 @@ namespace Train
         public Form1()
         {
             InitializeComponent();
+
+            arrivalHour.Text = Properties.Settings.Default.arrivalHour2.ToString();
+            arrivalMinute.Text = Properties.Settings.Default.arrivalMinute2.ToString();
+            departHour.Text = Properties.Settings.Default.departHour2.ToString();
+            departMinute.Text = Properties.Settings.Default.departMinute2.ToString();
+            commingHour.Text = Properties.Settings.Default.commingHour2.ToString();
+            commingMinute.Text = Properties.Settings.Default.commingMinute2.ToString();
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int arrivalHour1, arrivalMinute1, departHour1, departMinute1, commingHour1, commingMinute1;
+            try
+            {
+                arrivalHour1 = int.Parse(this.arrivalHour.Text);
+                arrivalMinute1 = int.Parse(this.arrivalMinute.Text);
+                departHour1 = int.Parse(this.departHour.Text);
+                departMinute1 = int.Parse(this.departMinute.Text);
+                commingHour1 = int.Parse(this.commingHour.Text);
+                commingMinute1 = int.Parse(this.commingMinute.Text);
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+
+            Properties.Settings.Default.arrivalHour2 = arrivalHour1;
+            Properties.Settings.Default.arrivalMinute2 = arrivalMinute1;
+            Properties.Settings.Default.departHour2 = departHour1;
+            Properties.Settings.Default.departMinute2 = departMinute1;
+            Properties.Settings.Default.commingHour2 = commingHour1;
+            Properties.Settings.Default.commingMinute2 = commingMinute1;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(Logic.WillStay(arrivalHour1, arrivalMinute1, departHour1, departMinute1, commingHour1, commingMinute1));
+        }
+    }
+    public class Logic
+    {
+        public static string WillStay(int arrivalHour, int arrivalMinute, int departHour, int departMinute, int commingHour, int commingMinute)
+        {
+            string outMessage = "";
+            if (((arrivalHour < commingHour) || ((arrivalHour == commingHour) && (arrivalMinute <= commingMinute))) &&
+                ((departHour > commingHour) || ((departHour == commingHour) && (departMinute > commingMinute))))
+            {
+                outMessage = "Поезд будет стоять на платформе";
+            }
+            else
+            {
+                outMessage = "Поезд не будет стоять на платформе";
+            }
+            return outMessage;
+        }
+
+
     }
 }
